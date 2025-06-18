d20fd3be62c8183ac1b10452c9651dcd54c4486a
UrielGomezG <20233tn177@utez.edu.mx>
2025-06-17 21:55:12 -0600

    🧪[TEST] Agregue un usuario creado como prueba para el inicio de sesion

---
+++ ObjetosPerdidos/src/main/java/com/utez/objetosperdidos/util/Session.java
@@ -0,0 +1,14 @@
+package com.utez.objetosperdidos.util;
+
+import com.utez.objetosperdidos.model.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Session {
+    public static ObservableList<User> users = FXCollections.observableArrayList();
+    static {
+        // Initialize with some dummy data
+        users.add(new User("Admin", "Mauro@utez.edu", "12345", "77711223344", "20233tn117"));
+    }
+
+}

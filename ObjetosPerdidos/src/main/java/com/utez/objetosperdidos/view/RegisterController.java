d806f984d93fede3ce937b597b64cb952cb3d4a7
UrielGomezG <20233tn177@utez.edu.mx>
2025-06-20 17:07:44 -0600

    ⚡️Validacion de campos en el registro

--- ObjetosPerdidos/src/main/java/com/utez/objetosperdidos/view/RegisterController.java
+++ ObjetosPerdidos/src/main/java/com/utez/objetosperdidos/view/RegisterController.java
@@ -14,12 +14,43 @@ public class RegisterController {
 
     @FXML public void onSignUp() throws Exception {
 
-        if (nameField.getText().isEmpty() || matriculaField.getText().isEmpty()
-            || emailField.getText().isEmpty() || passwordField.getText().isEmpty()) {
-            messageLabel.setText("Completa todos los campos.");
-            return;
-        }
+        String nombre = nameField.getText();
+        String matricula = matriculaField.getText();
+        String telefono = phoneField.getText();
+        String email = emailField.getText();
+        String password = passwordField.getText();
 
+        if (nombre.isEmpty() || matricula.isEmpty() || telefono.isEmpty() || email.isEmpty() || password.isEmpty()) {
+        messageLabel.setText("Completa todos los campos.");
+        return;
+    }
+
+    if (!nombre.matches("^[A-Za-z]+$")) {
+        messageLabel.setText("El nombre solo debe contener letras.");
+        return;
+    }
+
+    if (!matricula.matches("^[a-zA-Z0-9]+$")) {
+        messageLabel.setText("La matrícula solo debe contener letras y números.");
+        return;
+    }
+
+    if (!telefono.matches("^\\d{10}$")) {
+        messageLabel.setText("El teléfono debe tener exactamente 10 dígitos.");
+        return;
+    }
+
+    if (!email.matches("^[\\w.-]+@utez\\.edu\\.mx$")) {
+        messageLabel.setText("El correo debe ser institucional (@utez.edu.mx).");
+        return;
+    }
+
+    if (password.length() < 5) {
+        messageLabel.setText("La contraseña debe tener al menos 5 caracteres.");
+        return;
+    }
+
+        messageLabel.setText("Registro válido.");
         Main.switchScene("PrivacyView.fxml");
     }
 

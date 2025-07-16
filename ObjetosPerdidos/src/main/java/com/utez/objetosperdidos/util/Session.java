6884bc434195f5cba5b498a5b2188568184a7a3d
UrielGomezG <20233tn177@utez.edu.mx>
2025-07-15 23:01:45 -0600

    🔥Elimine el usuario que se tenia de forma local al ya no ser necesario

--- ObjetosPerdidos/src/main/java/com/utez/objetosperdidos/util/Session.java
+++ ObjetosPerdidos/src/main/java/com/utez/objetosperdidos/util/Session.java
@@ -8,10 +8,5 @@ import javafx.collections.ObservableList;
 public class Session {
     public static ObservableList<User> users = FXCollections.observableArrayList();
     public static User currentUser = null;
-    
-    static {
-
-        users.add(new User("Admin", "Mauro@utez.edu", "12345", "77711223344", "20233tn117","admin"));
-    }
 
 }

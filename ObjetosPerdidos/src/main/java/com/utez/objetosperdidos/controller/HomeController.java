cffc097c5b8af3cb0db01f63bd9ad17d38ad3f28
UrielGomezG <20233tn177@utez.edu.mx>
2025-07-23 00:14:45 -0600

    ✨ Boceto del como se ven las cards con algunos objetos de prueba (sin imagenes)

--- ObjetosPerdidos/src/main/java/com/utez/objetosperdidos/controller/HomeController.java
+++ ObjetosPerdidos/src/main/java/com/utez/objetosperdidos/controller/HomeController.java
@@ -1,5 +1,29 @@
 package com.utez.objetosperdidos.controller;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
 public class HomeController {
-    // Puedes dejarlo vacío por ahora si no necesitas lógica
+@FXML
+private BorderPane mainContainer;
+
+@FXML
+public void initialize() {
+    mostrarVistaObjetos();
+}
+
+public void mostrarVistaObjetos() {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/utez/objetosperdidos/view/objetos.fxml"));
+        Parent vista = loader.load();
+        mainContainer.setCenter(vista);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
 }

07fb18ee1ac0b393e085429e540fe2a0dced4c7e
UrielGomezG <20233tn177@utez.edu.mx>
2025-07-28 18:28:23 -0600

    ➕Ayudan que el formulario del agregar objetos, muestren los estados y edificos que hay en la base de datos

---
+++ ObjetosPerdidos/src/main/java/com/utez/objetosperdidos/model/Edificio.java
@@ -0,0 +1,19 @@
+package com.utez.objetosperdidos.model;
+
+public class Edificio {
+    private int id;
+    private String nombre;
+
+    public Edificio(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() { return id; }
+    public String getNombre() { return nombre; }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
---
+++ ObjetosPerdidos/src/main/java/com/utez/objetosperdidos/model/Estado.java
@@ -0,0 +1,19 @@
+package com.utez.objetosperdidos.model;
+
+public class Estado {
+    private int id;
+    private String nombre;
+
+    public Estado(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() { return id; }
+    public String getNombre() { return nombre; }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}

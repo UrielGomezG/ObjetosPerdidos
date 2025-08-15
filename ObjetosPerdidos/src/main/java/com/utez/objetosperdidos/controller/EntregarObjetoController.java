a6e16ce442b1b30e054b6bf17915bb105708ad6b
Gabriel-Medina-coder <20243ds040@utez.edu.mx>
2025-08-14 21:38:50 -0600

    ⚡️ Estilo

--- ObjetosPerdidos/src/main/java/com/utez/objetosperdidos/controller/EntregarObjetoController.java
+++ ObjetosPerdidos/src/main/java/com/utez/objetosperdidos/controller/EntregarObjetoController.java
@@ -36,12 +36,10 @@ public class EntregarObjetoController {
     private TextField txtAlumno;
     @FXML
     private TextField txtMatricula;
-    @FXML
-    private Label lblAula;
-    @FXML
-    private Label lblEdificio;
+
     @FXML
     private Label lblNombreFoto;
+
     @FXML
     private TextField fotoUrlField;
 
@@ -57,16 +55,6 @@ public class EntregarObjetoController {
     public void setObjeto(ObjetoPerdido objeto) {
         this.objeto = objeto;
 
-        lblTitulo.setText(objeto.getNombreObjeto());
-        lblMarca.setText(objeto.getMarca());
-        lblModelo.setText(objeto.getModelo());
-        fotoUrlField.setText(objeto.getFotoUrl());
-        lblNoSerie.setText(objeto.getNo_serial());
-        lblDescripcion.setText(objeto.getDescripcion());
-        lblAula.setText(objeto.getAula());
-        lblEdificio.setText(objeto.getEdificio());
-
-
         if(objeto != null){
             lblTitulo.setText(objeto.getNombreObjeto());
             lblMarca.setText(objeto.getMarca());
@@ -75,7 +63,7 @@ public class EntregarObjetoController {
             lblNoSerie.setText(objeto.getNo_serial());
             lblDescripcion.setText(objeto.getDescripcion());
         }
-
+        
     }
 
     @FXML
